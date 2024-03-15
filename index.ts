337a27f8f69db1bf93be365fa5f4244a375dc448
Artem Adeev <artemadeev42@gmail.com>
2024-03-15 11:00:15 +0300

    fix production

--- index.ts
+++ index.ts
@@ -14,7 +14,7 @@ const app: Application = express();
 
 const PORT: number = process.env.PORT ? parseInt(process.env.PORT) : 8000;
 
-const DB_URL: string = process.env.DB_URL ? process.env.DB_URL : "mongodb://localhost:27017/shopdb"
+const DB_URL: string = process.env.DB_URL ? process.env.DB_URL : ''
 
 const corsOptions: CorsOptions = {
     origin: process.env.CLIENT_URL,
